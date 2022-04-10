c92abaac4a46d5145997b2ba6e5b070de115452f
olesya <olegha2000@gmail.com>
2022-04-10 22:40:23 +0600

    мьют функция

--- const.py
+++ const.py
@@ -1,18 +1,2 @@
-import discord
-import os
+HELLO_LIST = [".", "Привет!", "привет!", "Привет", "привет", "Салам алейкум", "hello", "guten tag"]
 
-client = discord.Client()
-TOKEN = ''
-
-hello_list = ["Привет!", "привет!", "Привет", "привет", "Салам алейкум", "hello", "guten tag"]
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    for hello in hello_list:
-        if message.content.startswith(hello):
-            await message.channel.send('Мяу...')
-
-client.run(TOKEN)
---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import discord.utils
+import os
+from const import *
+
+
+with open("token.txt", "r") as f:
+    TOKEN = f.read()
+
+
+class MyClient(discord.Client):
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        for hello in HELLO_LIST:
+            if message.content.startswith(hello):
+                await message.channel.send('Мяу...')
+
+        if message.content.startswith("!mute"):
+            role = discord.utils.get(message.guild.roles, id=962041876041564240)
+            user = message.mentions[0]
+            await user.add_roles(role)
+            await message.channel.send(f"Доступ к текстовому каналу для {user} ограничен")
+
+        if message.content.startswith("!unmute"):
+            role = discord.utils.get(message.guild.roles, id=962041876041564240)
+            user = message.mentions[0]
+            await user.remove_roles(role)
+            await message.channel.send(f"Доступ к текстовому каналу для {user} возобновлен")
+
+
+
+client = MyClient()
+
+client.run(TOKEN)
