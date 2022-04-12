6b81201cfd7e551b606ae349d47cc880c8626f1e
olesya <olegha2000@gmail.com>
2022-04-13 00:01:57 +0600

    немного модерации

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import discord.utils
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 import os
 from const import *
 
@@ -9,6 +11,34 @@ with open("token.txt", "r") as f:
 
 
 class MyClient(discord.Client):
+    async def warning(self, message, author=False):
+        if author:
+            user = message.author
+        else:
+            user = message.mentions[0]
+
+        if user in MEMBERS.keys():
+            MEMBERS[user] += 1
+        else:
+            MEMBERS[user] = 1
+        await message.channel.send(f"У {user} {MEMBERS[user]} из 5 предупреждений")
+
+        if MEMBERS[user] == 5:
+            await message.channel.send(f"{user} достиг(ла) максимального количества предупреждений")
+            await self.mute(message)
+
+    async def mute(self, message):
+        role = discord.utils.get(message.guild.roles, id=962041876041564240)
+        user = message.mentions[0]
+        await user.add_roles(role)
+        await message.channel.send(f"Доступ к текстовому каналу для {user} ограничен")
+
+    async def unmute(self, message):
+        role = discord.utils.get(message.guild.roles, id=962041876041564240)
+        user = message.mentions[0]
+        await user.remove_roles(role)
+        await message.channel.send(f"Доступ к текстовому каналу для {user} возобновлен")
+
     async def on_message(self, message):
         if message.author == client.user:
             return
@@ -18,16 +48,20 @@ class MyClient(discord.Client):
                 await message.channel.send('Мяу...')
 
         if message.content.startswith("!mute"):
-            role = discord.utils.get(message.guild.roles, id=962041876041564240)
             user = message.mentions[0]
-            await user.add_roles(role)
-            await message.channel.send(f"Доступ к текстовому каналу для {user} ограничен")
+            await self.mute(user)
 
         if message.content.startswith("!unmute"):
-            role = discord.utils.get(message.guild.roles, id=962041876041564240)
             user = message.mentions[0]
-            await user.remove_roles(role)
-            await message.channel.send(f"Доступ к текстовому каналу для {user} возобновлен")
+            await self.unmute(user)
+
+        if message.content.startswith("!warning"):
+            await self.warning(message)
+
+        if fuzz.partial_ratio(message.content.lower(), BAD_WORDS) >= 50:
+            await message.channel.send("На этом канале не ругаются>:(")
+            await self.warning(message, author=True)
+            await message.delete()
 
 
 
