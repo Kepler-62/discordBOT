95e3c8f47a3b3dcfc4c1f68315140ec5c346a3d4
olesya <olegha2000@gmail.com>
2022-04-25 22:14:22 +0600

    апишки всякие

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import discord.utils
 from fuzzywuzzy import fuzz
+import requests
+import yandex_weather_api
+import json
 from fuzzywuzzy import process
 import os
 from const import *
@@ -11,6 +14,32 @@ with open("token.txt", "r") as f:
 
 
 class MyClient(discord.Client):
+    async def spam(self, message):
+        for channel in self.get_all_channels():
+            print(channel)
+            try:
+                await channel.send(message.content[5:])
+            except Exception:
+                continue
+
+    async def weather(self, message):
+        weather_token = "67faa5a552a1f272e9b244ae2a696b8d"
+        try:
+            city = message.content.split()[1]
+            request = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric")
+            data = request.json()
+
+            city = data['name']
+            current_weather = data['main']['temp']
+            water = data['main']['humidity']
+            pressure = data['main']['pressure']
+            wind = data['wind']['speed']
+
+            await message.channel.send(f"Погода в городе {city}: \nТемпература: {current_weather}\nВлажность: {water}\nДавление: {pressure}\nСкорость ветра: {wind}")
+        except Exception:
+            await message.channel.send("Ты что-то не так написал")
+
+
     async def warning(self, message, author=False):
         if author:
             user = message.author
@@ -39,6 +68,12 @@ class MyClient(discord.Client):
         await user.remove_roles(role)
         await message.channel.send(f"Доступ к текстовому каналу для {user} возобновлен")
 
+    async def choice(self, message):
+        request = requests.get("https://yesno.wtf/api")
+        response = request.json()
+        print(response)
+        await message.channel.send(response['image'])
+
     async def on_message(self, message):
         if message.author == client.user:
             return
@@ -49,20 +84,27 @@ class MyClient(discord.Client):
 
         if message.content.startswith("!mute"):
             user = message.mentions[0]
-            await self.mute(user)
+            await self.mute(message)
 
         if message.content.startswith("!unmute"):
-            user = message.mentions[0]
-            await self.unmute(user)
+            await self.unmute(message)
 
         if message.content.startswith("!warning"):
             await self.warning(message)
 
-        if fuzz.partial_ratio(message.content.lower(), BAD_WORDS) >= 50:
+        if message.content.startswith("!spam"):
+            await self.spam(message)
+
+        if message.content.startswith("!choice"):
+            await self.choice(message)
+
+        if message.content.startswith("!weather"):
+            await self.weather(message)
+
+        if fuzz.partial_ratio(message.content.lower(), BAD_WORDS) >= 60:
             await message.channel.send("На этом канале не ругаются>:(")
             await self.warning(message, author=True)
             await message.delete()
-
 
 
 client = MyClient()
